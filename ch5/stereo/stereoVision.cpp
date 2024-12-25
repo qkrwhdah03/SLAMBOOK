7df1d3c9cbbfb9c33c3f193d9c81ac25598302a5
qkrwhdah03 <qkrwhdah03@naver.com>
2024-12-26 00:42:12 +0900

    [ch5] stereo done

--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -3,46 +3,37 @@
 #include <string>
 #include <Eigen/Core>
 #include <pangolin/pangolin.h>
-#include <unistd.h>
+#include <windows.h>
 
 using namespace std;
 using namespace Eigen;
 
-// 文件路径
-string left_file = "./left.png";
-string right_file = "./right.png";
+string left_file = "C:/Users/qkrwh/OneDrive/Desktop/VClab/visual-slam-2024-fall-indi2-code/revision/ch5/stereo/left.png";
+string right_file = "C:/Users/qkrwh/OneDrive/Desktop/VClab/visual-slam-2024-fall-indi2-code/revision/ch5/stereo/right.png";
 
-// 在pangolin中画图，已写好，无需调整
-void showPointCloud(
-    const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
 
 int main(int argc, char **argv) {
 
-    // 内参
     double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
-    // 基线
     double b = 0.573;
 
-    // 读取图像
     cv::Mat left = cv::imread(left_file, 0);
     cv::Mat right = cv::imread(right_file, 0);
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
-        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32); 
     cv::Mat disparity_sgbm, disparity;
     sgbm->compute(left, right, disparity_sgbm);
     disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
 
-    // 生成点云
     vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
 
-    // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
     for (int v = 0; v < left.rows; v++)
         for (int u = 0; u < left.cols; u++) {
             if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
 
-            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
+            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); 
 
-            // 根据双目模型计算 point 的位置
             double x = (u - cx) / fx;
             double y = (v - cy) / fy;
             double depth = fx * b / (disparity.at<float>(v, u));
@@ -55,7 +46,6 @@ int main(int argc, char **argv) {
 
     cv::imshow("disparity", disparity / 96.0);
     cv::waitKey(0);
-    // 画出点云
     showPointCloud(pointcloud);
     return 0;
 }
@@ -95,7 +85,7 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &
         }
         glEnd();
         pangolin::FinishFrame();
-        usleep(5000);   // sleep 5 ms
+        Sleep(50);   // sleep 50 ms
     }
     return;
 }
