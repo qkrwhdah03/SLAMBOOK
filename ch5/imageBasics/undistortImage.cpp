0f67dc3d0704b51e2dce250f87ccce0e81330016
qkrwhdah03 <qkrwhdah03@naver.com>
2025-01-06 12:21:30 +0900

    [ch5] undistortImage averaging added

--- ch5/imageBasics/undistortImage.cpp
+++ ch5/imageBasics/undistortImage.cpp
@@ -1,9 +1,69 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
+#define DEFAULT 0
+#define AVG 1
+#define L1 2
+#define L2 3
+
 using namespace std;
 
 string image_file = "C:/Users/qkrwh/OneDrive/Desktop/VClab/visual-slam-2024-fall-indi2-code/revision/ch5/imageBasics/distorted.png";
+
+
+uchar calculate_pixel(cv::Mat image, double v_distorted, double u_distorted, int mode){
+  int y = (int) v_distorted;
+  int x = (int) u_distorted;
+  int rows = image.rows;
+  int cols = image.cols;
+  
+  if(mode == DEFAULT) {
+    return image.at<uchar>(y, x);
+  }
+  if(mode == AVG){ 
+    double val = 0;
+    int cnt = 0;
+    for(int i=x; i<=x+1; ++i){
+      for(int j=y; j<=y+1; ++j){
+        if(i >= cols || j >= rows) continue;
+        val += image.at<uchar>(j,i);
+        cnt++;
+      }
+    }
+    return (uchar)(val/cnt);
+  }
+  if(mode == L1){ // Weighted average of neighboring pixels based on L2 distance
+    double val = 0;
+    double cnt = 0;
+    for(int i=x; i<=x+1; ++i){
+      for(int j=y; j<=y+1; ++j){
+        if(i >= cols || j >= rows) continue;
+        double dx = cv::abs(i-u_distorted);
+        double dy = cv::abs(j-v_distorted);
+        double dis = dx + dy;
+        val += 1/dis * image.at<uchar>(j,i);
+        cnt += 1/dis;
+      }
+    }
+    return (uchar)(val/cnt);
+  }
+  if(mode == L2){ // Weighted average of neighboring pixels based on L2 distance
+    double val = 0;
+    double cnt = 0;
+    for(int i=x; i<=x+1; ++i){
+      for(int j=y; j<=y+1; ++j){
+        if(i >= cols || j >= rows) continue;
+        double dx = i-u_distorted;
+        double dy = j-v_distorted;
+        double dis = cv::sqrt(dx*dx + dy*dy);
+        val += 1/dis * image.at<uchar>(j,i);
+        cnt += 1/dis;
+      }
+    }
+    return (uchar)(val/cnt);
+  }
+  else return (uchar) 0;
+}
 
 int main(int argc, char **argv) {
 
@@ -12,7 +72,7 @@ int main(int argc, char **argv) {
 
   cv::Mat image = cv::imread(image_file, 0);
   int rows = image.rows, cols = image.cols;
-  cv::Mat image_undistort = cv::Mat(rows, cols, CV_8UC1);
+  cv::Mat image_undistort = cv::Mat(rows, cols, CV_8UC1); // unsigned int with channel 1
 
   for (int v = 0; v < rows; v++) {
     for (int u = 0; u < cols; u++) {
@@ -24,7 +84,7 @@ int main(int argc, char **argv) {
       double v_distorted = fy * y_distorted + cy;
 
       if (u_distorted >= 0 && v_distorted >= 0 && u_distorted < cols && v_distorted < rows) {
-        image_undistort.at<uchar>(v, u) = image.at<uchar>((int) v_distorted, (int) u_distorted);
+        image_undistort.at<uchar>(v, u) = calculate_pixel(image, v_distorted, u_distorted, L1);
       } else {
         image_undistort.at<uchar>(v, u) = 0;
       }
